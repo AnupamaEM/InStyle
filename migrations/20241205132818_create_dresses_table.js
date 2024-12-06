d0502821c41c9fafb844dc96d2eabc1c2472387d
AnupamaEM <anupamaem923@gmail.com>
2024-12-06 17:02:17 +0530

    dropping and recreating table during migration resolved

--- migrations/20241205132818_create_dresses_table.js
+++ migrations/20241205132818_create_dresses_table.js
@@ -1,22 +1,21 @@
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
     return knex.schema.createTable('dresses', (table) => {
         table.increments('id').primary(); 
         table.string('name').notNullable(); 
         table.decimal('cost', 10, 2).notNullable(); 
         table.string('imagePath').notNullable(); 
-      });
-  
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTableIfExists('dresses'); 
-  
 };
