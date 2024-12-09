18434d42a20a3999421bdb94e3360303c9505c08
AnupamaEM <anupamaem923@gmail.com>
2024-12-09 11:02:04 +0530

    installed package cors

--- welcome.js
+++ welcome.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const knex = require('knex');
+const bodyParser = require('body-parser');
 const app = express(); 
 const PORT = 3000; 
+const cors = require('cors');
+
 app.use(express.json());
+app.use(cors()); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 const db = knex({
     client: 'mysql2',
